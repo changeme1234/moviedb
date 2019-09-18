c0be1a99e8398bdd788c3d00eb827c93cc8a886b
Angelo Suinan <suinanangelo@gmail.com>
2019-09-18 14:24:57 +0800

    :poop:  Add bootstrap settings screen with logout

--- src/views/Settings.js
+++ src/views/Settings.js
@@ -1,10 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import PropTypes from 'proptypes'
+import { View } from 'react-native'
+import { Button, Text } from 'native-base'
 
-export default function Settings () {
+import { removeSession } from '../storage'
+
+export default function Settings ({ navigation }) {
+  const logout = () => {
+    removeSession()
+    navigation.navigate('Auth')
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings screen</Text>
+      <Text>Settings Screen</Text>
+      <Button onPress={() => logout()}><Text>Logout</Text></Button>
     </View>
   )
+}
+
+Settings.propTypes = {
+  navigation: PropTypes.navigation,
 }
